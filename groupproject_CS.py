2264bf0b6786bc46bd35b94774a20c4c76cc5069
juliasgnr <162497246+juliasgnr@users.noreply.github.com>
2024-03-24 17:13:43 +0100

    numbers

--- groupproject_CS.py
+++ groupproject_CS.py
@@ -6,3 +6,12 @@ a = 27
 b = 30
 st.write(a+b)
 print("hello")
+
+calculate every even number between 37 and 198
+even_numbers = []
+for i in range(37, 199):
+    if i % 2 == 0:
+        even_numbers.append(i)
+
+print(even_numbers)
+
